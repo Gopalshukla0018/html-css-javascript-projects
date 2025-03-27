013d4c81d1bf6dc09d24cc6a6b8cc32e38a5e81a
Gopal Shukla <gopalshukla0018@gmail.com>
2025-03-27 13:52:06 +0530

    degital clock

---
+++ degital clock/script.js
@@ -0,0 +1,24 @@
+const time = document.getElementById("time");
+const timeformat = document.getElementById("timeformat");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setInterval(showtime, 1000);
+});
+
+const showtime = () => {
+  let date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let secs = date.getSeconds();
+  let milisecs = date.getMilliseconds();
+  hr = hr % 12 || 12;
+
+  if ( date.getHours()>= 12) timeformat.innerText = "pm";
+  else timeformat.innerText = "am";
+
+  if (hr < 10) hr = "0" + hr;
+  if (min < 10) min = "0" + min;
+  if (secs < 10) secs = "0" + secs;
+
+  time.innerHTML = `${hr} : ${min} :${secs}`;
+};
